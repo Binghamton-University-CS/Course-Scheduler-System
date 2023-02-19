f18bb2226e85013dc4635176b14ada30ce4a08ef
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-19 18:52:14 -0500

    Update scheduler.cpp

--- scheduler.cpp
+++ scheduler.cpp
@@ -15,10 +15,12 @@ void processInputAndPrint(vector<string>);
 
 int main() {
 	string input;
+	Course* courseListPointer = new MichaelList<Course>();
+	Student* studentListPointer = new MichaelList<Student>();
 	while (true){
 		showPrompt();
 		getline(cin, input);
-		processInputAndPrint(getTokenListFromString(input));
+		processInputAndPrint(getTokenListFromString(input), courseListPointer, studentListPointer);
 	}
 	return 0;
 }
@@ -53,7 +55,7 @@ vector<string> getTokenListFromString(string input) {
 	return tL;
 }
 
-void processInputAndPrint(vector<string> tL) {
+void processInputAndPrint(vector<string> tL, Course* cLP, Student* sLP) {
 	if (tL.size() == 0) {
 		cout << "Command not recognized, please try again." << endl;
 	}
@@ -67,7 +69,10 @@ void processInputAndPrint(vector<string> tL) {
 			if (checkCRN(tL.at(1)) {
 				if (checkDepartment(tL.at(2)) {
 					if (checkNumber(tL.at(3)) {
-						cout << "build course " << tL.at(1) << " " << tL.at(2) << " " << tL.at(3) << " "<< tL.at(4) << endl;
+						if (checkName(tL.at(4)) {
+							cLP->add(new Course(tL.at(1), tL.at(2), tL.at(3), tL.at(4)));
+							cout << "build course " << tL.at(1) << " " << tL.at(2) << " " << tL.at(3) << " "<< tL.at(4) << endl;
+						}
 					}
 				}
 			}
@@ -78,6 +83,7 @@ void processInputAndPrint(vector<string> tL) {
 		checkExtraArguments(tL.size(), 2);
 		if (tL.size() >= 2) {
 			if (checkCRN(tL.at(1)) {
+				cLP->remove(tL.at(1));
 				cout << "Success: cancelled course " << tL.at(1) << endl;
 			}
 		}
@@ -90,6 +96,7 @@ void processInputAndPrint(vector<string> tL) {
 				if (checkUserID(tL.at(2))) {
 					if (checkFirst(tL.at(3))) {
 						if (checkLast(tL.at(4))) {
+							sLP->add(tL.at(1), tL.at(2), tL.at(3), tL.at(4));
 							cout << "enroll student " << tL.at(1) << " (" << tL.at(2) << ") " << tL.at(4) << ", " << tL.at(3) << endl;
 						}
 					}
@@ -103,6 +110,7 @@ void processInputAndPrint(vector<string> tL) {
 		if (tL.size() == 3) {
 			if (checkBNumber(tL.at(1))) {
 				if (checkCRN(tL.at(2))) {
+					cLP->find(tL.at(2)).add_____
 					cout << "add student " << tL.at(1) << " into course " << tL.at(2) << endl;
 				}
 			}
