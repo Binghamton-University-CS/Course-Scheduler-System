2520a2eba75e8b95a664b5f1cd1626ac3dab9da7
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-19 18:02:24 -0500

    Update Course.cpp

--- Course.cpp
+++ Course.cpp
@@ -1,9 +1,16 @@
 #include <string>
-#include "Course.h"
 
-Course::Course(string CRN, string department, string number, string name) {
-  this->CRN = CRN;
-  this->department = department;
-  this->number = number;
-  this->name = name;
-}
+class Course {
+  public:
+    Course(string CRN, string department, string number, string name) {
+      this->CRN = CRN;
+      this->department = department;
+      this->number = number;
+      this->name = name;
+    }
+  private:
+    string CRN;
+    string department;
+    string number;
+    string name;
+};
