594fbbaffc2efa3b141262ab03d1ad60ef0f465e
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-19 14:06:11 -0500

    Create Student.cpp

---
+++ Student.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include "Student.h"
+
+Student::Student(string first, string last, string userID, string bNumber) {
+  this->first = first;
+  this->last = last;
+  this->userID = userID;
+  this->bNumber = bNumber;
+}
