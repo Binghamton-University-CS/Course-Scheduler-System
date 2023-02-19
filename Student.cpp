39974a67af46e687cc18755fa9653f8fffee8ac5
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-19 18:03:54 -0500

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,9 +1,16 @@
 #include <string>
-#include "Student.h"
 
-Student::Student(string BNumber, string userID, string first, string last) {
-  this->BNumber = BNumber;
-  this->userID = userID;
-  this->first = first;
-  this->last = last;
-}
+class Student {
+  public:
+    Student(string BNumber, string userID, string first, string last) {
+      this->BNumber = BNumber;
+      this->userID = userID;
+      this->first = first;
+      this->last = last;
+    }
+  private:
+    string BNumber;
+    string userID;
+    string first;
+    string last;  
+};
