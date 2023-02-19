81a68c0a7fc26b76029117da5d69ee7508e7df6b
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-19 18:05:16 -0500

    Update MichaelList.cpp

--- MichaelList.cpp
+++ MichaelList.cpp
@@ -1,21 +1,9 @@
-#include "MichaelList.h"
-
-MichaelList::MichaelList(string type) {
-  if (type == "Student") {
-    array = new Student[100];
-  }
-  else if (type == "Course") {
-    array = new Course[100];
-  }
-  else {
-    cout << "Error, invalid type" << endl;
-  }
-}
-
-MichaelList::add() {
-  
-}
-
-MichaelList::remove() {
-  
-}
+class MichaelList {
+  public:
+    MichaelList();
+    add();
+    remove();
+  private:
+    int* array;
+    int length;
+};
