3666954165b0bade8e7c33d6b21918a759ae5a9a
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-19 18:19:03 -0500

    Update MichaelList.cpp

--- MichaelList.cpp
+++ MichaelList.cpp
@@ -1,9 +1,27 @@
+template <class T>
 class MichaelList {
   public:
-    MichaelList();
-    add();
-    remove();
+    MichaelList() {
+      array = new T[size];
+    }
+    add(T val) {
+      if (lastIndex == size - 1) {
+        T* temp = new T[size * 2];
+        for (int i = 0; i < size; i++) {
+          temp[i] = array[i];
+        }
+        delete array;
+        array = temp;
+        size *= 2;
+      }
+      lastIndex++;
+      array[lastIndex] = val;
+    }
+    remove(T val) {
+      
+    }
   private:
-    int* array;
-    int length;
+    T* array;
+    int size = 100;
+    int lastIndex;
 };
