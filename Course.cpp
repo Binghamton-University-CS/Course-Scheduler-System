2f8fee18cab1de64982d004349d89c6ec167638b
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-19 18:56:52 -0500

    Update Course.cpp

--- Course.cpp
+++ Course.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include "MichaelList.cpp"
 
 class Course {
   public:
@@ -7,10 +8,18 @@ class Course {
       this->department = department;
       this->number = number;
       this->name = name;
+      BNumbers = new MichaelList<string>();
+    }
+    addStudent(string BNumber) {
+      studentList.add(BNumber);
+    }
+    dropStudent(string BNumber) {
+      studentList.remove(BNumber);
     }
   private:
     string CRN;
     string department;
     string number;
     string name;
+    MichaelList<string> studentList;
 };
