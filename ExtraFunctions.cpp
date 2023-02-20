c4e6d4f0ce39f86019e57034cefe2461fa5bd9ed
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-19 19:19:32 -0500

    Update ExtraFunctions.cpp

--- ExtraFunctions.cpp
+++ ExtraFunctions.cpp
@@ -2,6 +2,20 @@
 #include <string>
 #include <iostream>
 
+void printCourseRoster(string CRN, Course* courseList, Student* studentList) {
+	Course course = courseList->find(CRN);
+	MichaelList<string> BNumbers = course.getStudents();
+	cout << "CRN: " << CRN << endl;
+	cout << "Students: " << BNumbers.getSize() << endl;
+	for (int i = 0; i < BNumbers.getSize(); i++) {
+		cout << BNumbers.at(i) << studentList->find(BNumbers.at(i)).getName() << endl;
+	}
+}
+
+void printStudentSchedule(string BNumber, Course* courseList, Student* studentList) {
+	Student student = studentList->find(BNumber);
+}
+
 void checkExtraArguments(int val, int max) {
 	if (val > max) {
 		cout << "Warning: ignoring extra argument(s)" << endl;
