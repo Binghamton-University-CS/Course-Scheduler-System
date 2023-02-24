0152939bc91e77b119bc06701bb77322a1e61e8e
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-24 09:27:20 -0500

    Create StudentList.cpp

---
+++ StudentList.cpp
@@ -0,0 +1,31 @@
+#ifndef STUDENT
+#define STUDENT
+#include "Student.cpp"
+#endif
+
+class StudentList {
+  public:
+    StudentList() {
+      array = new T[size];
+    }
+    add(Student student) {
+      if (lastIndex == size - 1) {
+        T* temp = new T[size * 2];
+        for (int i = 0; i < size; i++) {
+          temp[i] = array[i];
+        }
+        delete array;
+        array = temp;
+        size *= 2;
+      }
+      lastIndex++;
+      array[lastIndex] = student;
+    }
+    remove(Student student) {
+      
+    }
+  private:
+    Student* array;
+    int size = 100;
+    int lastIndex;
+};
