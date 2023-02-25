c0114b8b3c8bc20f7f2d2058c67af6130bd4737c
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-25 10:19:54 -0500

    Update MichaelList.cpp

--- MichaelList.cpp
+++ MichaelList.cpp
@@ -4,7 +4,11 @@ class MichaelList {
     MichaelList() {
       array = new T[size];
     }
+    
     add(T val) {
+      if (this->find(val) != -1) {
+        return false;
+      }
       if (lastIndex == size - 1) {
         T* temp = new T[size * 2];
         for (int i = 0; i < size; i++) {
@@ -17,8 +21,25 @@ class MichaelList {
       lastIndex++;
       array[lastIndex] = val;
     }
-    remove(T val) {
-      
+    
+    bool remove(T val) {
+      int index = this->find(val);
+      if (index == -1) {
+        return false;
+      }
+      for (int i = index; i < lastIndex - 1; i++) {
+        array[i] = array[i + 1];
+      }
+      return true;
+    }
+    
+    int find(T val) {
+      for (int i = 0; i < lastIndex; i++) {
+        if (val == array[i]) {
+          return i;
+        }
+      }
+      return -1;
     }
   private:
     T* array;
