06cafbb0ce8cfb2aadbecd66f8c7dc5037924058
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-25 12:50:47 -0500

    Create CourseList.h

---
+++ CourseList.h
@@ -0,0 +1,16 @@
+#ifndef COURSE_H
+#define COURSE_H
+#include "Course.h"
+#endif
+
+class CourseList {
+  public:
+    CourseList();
+    bool add(Course course);
+    bool remove(Course course);
+    int find(Course course);
+  private:
+    Course* array;
+    int size;
+    int lastIndex;
+};
