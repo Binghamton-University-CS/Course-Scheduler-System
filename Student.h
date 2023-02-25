99b0201ea0b5705208fda24a4ca078aa50528725
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-25 12:33:06 -0500

    Update Student.h

--- Student.h
+++ Student.h
@@ -10,18 +10,18 @@
 #include "MichaelList.h"
 #endif
 
-//using namespace std;
+using namespace std;
 
 class Student {
   public:
-    Student(std::string BNumber, std::string userID, std::string first, std::string last);
+    Student(string BNumber, string userID, string first, string last);
     void addCourse(Course course);
     void removeCourse(Course course);
     void printCourses();
   private:
-    std::string BNumber;
-    std::string userID;
-    std::string first;
-    std::string last; 
+    string BNumber;
+    string userID;
+    string first;
+    string last; 
     MichaelList<Course> courseList;
 };
