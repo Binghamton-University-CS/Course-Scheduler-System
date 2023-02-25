afb9de1e3545af527987ee6f0120d87054834ae0
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-25 15:51:55 -0500

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,27 +1,17 @@
 #include <string>
 
-#ifndef COURSE_H
-#define COURSE_H
-#include "Course.h"
-#endif
-
-#ifndef COURSELIST_H
-#define COURSELIST_H
-#include "CourseList.h"
-#endif
-
 using namespace std;
 
 class Student {
   public:
     Student(string BNumber, string userID, string first, string last);
-    void addCourse(Course course);
-    void removeCourse(Course course);
+    void addCourse(string course);
+    void removeCourse(string course);
     void printCourses();
   private:
     string BNumber;
     string userID;
     string first;
     string last; 
-    CourseList courseList;
+    StringList courseList;
 };
