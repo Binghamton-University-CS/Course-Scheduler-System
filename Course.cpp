bcb2f632490764bf7ada61fd814fc590fe39bbf4
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-25 11:04:47 -0500

    Update Course.cpp

--- Course.cpp
+++ Course.cpp
@@ -3,9 +3,9 @@
 #include "Student.h"
 #endif
 
-#ifndef STUDENTLIST_H
-#define STUDENTLIST_H
-#include "StudentList.h"
+#ifndef MICHAELLIST_H
+#define MICHAELLIST_H
+#include "MichaelList.h"
 #endif
 
 class Course {
@@ -15,7 +15,7 @@ class Course {
       this->department = department;
       this->number = number;
       this->name = name;
-      studentList = new StudentList();
+      studentList = new MichaelList<Student>();
     }
     string addStudent(Student student) {
       studentList.add(student);
@@ -31,5 +31,5 @@ class Course {
     string department;
     string number;
     string name;
-    StudentList studentList;
+    MichaelList<Student> studentList;
 };
