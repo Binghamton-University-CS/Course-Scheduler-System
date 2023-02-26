786b4d72f7bc23d5fe78077544852f5a52b5aa80
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-26 08:45:06 -0500

    Update Student.h

--- Student.h
+++ Student.h
@@ -9,7 +9,7 @@ using namespace std;
 
 class Student {
   public:
-    Student(string BNumber, string userID, string first, string last);
+    Student(string BNumber = "", string userID = "", string first = "", string last = "");
     void addCourse(string course);
     void removeCourse(string course);
     void printSchedule();
