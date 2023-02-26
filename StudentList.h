26330e5d599596ea1f207f82d0a3c6bb51da4ec7
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-25 22:22:55 -0500

    Update StudentList.h

--- StudentList.h
+++ StudentList.h
@@ -7,10 +7,9 @@ class StudentList {
   public:
     StudentList();
     bool add(Student student);
-    bool remove(Student student);
-    int find(Student student);
-    bool checkStudentExistsByBNumber(string BNumber);
-    Student getStudentByBNumber(string BNumber);
+    bool remove(string BNumber);
+    int find(string BNumber);
+    bool checkExists(string BNumber);
   private:
     Student* array;
     int size;
