b3b5ac01be2164d3cd2da5d8f06edeb6e18c7560
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-26 15:01:51 -0500

    Create ExtraFunctions.h

---
+++ ExtraFunctions.h
@@ -0,0 +1,15 @@
+#include <string>
+
+//void printCourseRoster(string CRN, Course* courseList, Student* studentList);
+//void printStudentSchedule(string BNumber, Course* courseList, Student* studentList;
+void checkExtraArguments(int val, int max);
+void checkNotEnoughArguments(int val, int min);
+bool regexCheck(string s, string e);
+bool checkCRN(string s);
+bool checkDepartment(string s);
+bool checkNumber(string s);
+bool checkBNumber(string s);
+bool checkUserID(string s);
+bool checkCourseName(string s);
+bool checkFirst(string s);
+bool checkLast(string s);
