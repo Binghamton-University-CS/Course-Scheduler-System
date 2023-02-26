696b41789ae09e216c4963b3d47ed619495a4732
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2023-02-25 22:45:06 -0500

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -17,7 +17,7 @@ Student::Student(string BNumber, string userID, string first, string last) {
       this->userID = userID;
       this->first = first;
       this->last = last;
-      courseList = new StringList();
+      courseList = StringList();
     }
 void Student::addCourse(Course course) {
       StringList.add(course);
